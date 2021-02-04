36278a0efb1a8d9104ffcbefcbcebf2fd7d7f4e3
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-02-04 18:25:32 +0900

    Initial Commit

---
+++ TaskPart1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskPart1
+//
+//  Created by niwa  shuhei on 2021/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
